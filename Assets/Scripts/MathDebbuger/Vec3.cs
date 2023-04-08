c2fb3d4214cc7ce39943bf52ee844e7bdd90931c
Dario <otrantodario@gmail.com>
2023-04-08 18:43:21 -0300

    Fix sqrMAgnitude

--- Assets/Scripts/MathDebbuger/Vec3.cs
+++ Assets/Scripts/MathDebbuger/Vec3.cs
@@ -11,7 +11,7 @@ namespace CustomMath
         public float y;
         public float z;
 
-        public float sqrMagnitude { get { return (x * x + y * y + z * z); } }
+        public float sqrMagnitude { get { return Mathf.Sqrt(x * x + y * y + z * z); } }
         public Vec3 normalized { get { return new Vec3(x / magnitude , y / magnitude , z / magnitude); } }
         public float magnitude { get { throw new NotImplementedException(); } }
         #endregion
