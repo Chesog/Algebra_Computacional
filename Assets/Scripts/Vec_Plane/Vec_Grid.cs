ef215c2f9accf6502d091d8634a6e12e30c0d383
Dario <otrantodario@gmail.com>
2023-05-09 17:36:00 -0300

    Add Vec_Grid Script

---
+++ Assets/Scripts/Vec_Plane/Vec_Grid.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomMath;
+
+public class Vec_Grid : MonoBehaviour
+{
+    public static Vec3[,,] v_Grid;
+    public int sizeX = 10;
+    public int sizeY = 10;
+    public int sizeZ = 10;
+    public bool showGrid;
+    public float delta = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v_Grid = new Vec3[sizeX,sizeY,sizeZ];
+
+        for (int x = 0; x < v_Grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < v_Grid.GetLength(1); y++)
+            {
+                for (int z = 0; z < v_Grid.GetLength(2); z++)
+                {
+                    // Lo multiplico por delta para que cada punto tenga una separacion
+                    v_Grid[x, y, z] = new Vec3(x,y,z) * delta;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+
+        if (!showGrid)
+            return;
+
+        for (int x = 0; x < v_Grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < v_Grid.GetLength(1); y++)
+            {
+                for (int z = 0; z < v_Grid.GetLength(2); z++)
+                {
+                    Gizmos.DrawWireSphere(v_Grid[x,y,z],0.1f);
+                }
+            }
+        }
+    }
+}
