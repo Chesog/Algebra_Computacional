88d86fa7866ed2e7d12ccdf49563d6aec95857f8
Dario <otrantodario@gmail.com>
2023-05-09 18:02:29 -0300

    Add MeshPlanes Creation
    create a new plane for every 3 vertex from the mesh

--- Assets/Scripts/Vec_Plane/Vec_MeshColider.cs
+++ Assets/Scripts/Vec_Plane/Vec_MeshColider.cs
@@ -28,17 +28,29 @@ public class Vec_MeshColider : MonoBehaviour
 
     public bool showV_MeshColider;
     public List<MeshPlanes> vec_Planes;
+    public MeshFilter objMesh;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        objMesh = GetComponent<MeshFilter>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        // i += 3 Por que Recorremos los puntos de 3 en 3 Para conseguir los vertices de la mesh
 
+        for (int i = 0; i < objMesh.mesh.GetIndices(0).Length; i += 3)
+        {
+            Vec3 aux1 = new Vec3 (objMesh.mesh.vertices[objMesh.mesh.GetIndices(0)[i]]);
+            Vec3 aux2 = new Vec3(objMesh.mesh.vertices[objMesh.mesh.GetIndices(0)[i + 1]]);
+            Vec3 aux3 = new Vec3 (objMesh.mesh.vertices[objMesh.mesh.GetIndices(0)[i + 2]]);
+
+            Vec_Plane auxP = new Vec_Plane(aux1, aux2, aux3);
+            vec_Planes.Add(new MeshPlanes(auxP,aux1,aux2,aux3));
+        }
     }
 }
