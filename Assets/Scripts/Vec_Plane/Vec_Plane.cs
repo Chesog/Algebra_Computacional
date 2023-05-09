234c1f2a431cba1775cde55a9aeb6cfa1b1a3923
Dario <otrantodario@gmail.com>
2023-05-09 18:14:31 -0300

    Add drawPlane Fuction

--- Assets/Scripts/Vec_Plane/Vec_Plane.cs
+++ Assets/Scripts/Vec_Plane/Vec_Plane.cs
@@ -109,5 +109,27 @@ namespace CustomMath
             float distanceToPoint2 = GetDistanceToPoint(inPt1);
             return (distanceToPoint > 0f && distanceToPoint2 > 0f) || (distanceToPoint <= 0f && distanceToPoint2 <= 0f);
         }
+
+        public void DrawPlane(Vec3 position, Vec3 normal, Color color)
+        {
+            Vector3 v3;
+            if (normal.normalized != Vector3.forward)
+                v3 = Vector3.Cross(normal, Vector3.forward).normalized * normal.magnitude;
+            else
+                v3 = Vector3.Cross(normal, Vector3.up).normalized * normal.magnitude; ;
+            var corner0 = position + v3;
+            var corner2 = position - v3;
+            var q = Quaternion.AngleAxis(90.0f, normal);
+            v3 = q * v3;
+            var corner1 = position + v3;
+            var corner3 = position - v3;
+            Debug.DrawLine(corner0, corner2, color);
+            Debug.DrawLine(corner1, corner3, color);
+            Debug.DrawLine(corner0, corner1, color);
+            Debug.DrawLine(corner1, corner2, color);
+            Debug.DrawLine(corner2, corner3, color);
+            Debug.DrawLine(corner3, corner0, color);
+            Debug.DrawRay(position, normal, Color.magenta);
+        }
     }
 }
