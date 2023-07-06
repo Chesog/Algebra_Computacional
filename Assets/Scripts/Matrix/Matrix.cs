130389fadf17088d33438a5993468260697c6825
Dario <otrantodario@gmail.com>
2023-07-06 15:45:26 -0300

    Add matrix this[int row, int column]

--- Assets/Scripts/Matrix/Matrix.cs
+++ Assets/Scripts/Matrix/Matrix.cs
@@ -13,8 +13,7 @@ namespace CustomMath
         public Vector4 col3;
         public Vector4 col4;
         #endregion
-        /*Vector4 operator (Matrix4x4 lhs, Vector4 vector)
-Matrix4x4 operator(Matrix4x4 lhs, Matrix4x4 rhs)*/
+
         #region Constructor
         public Matrix(Vector4 col1, Vector4 col2, Vector4 col3, Vector4 col4)
         {
@@ -28,15 +27,126 @@ Matrix4x4 operator(Matrix4x4 lhs, Matrix4x4 rhs)*/
         #region Operators
         private static readonly Matrix Zero = new Matrix(new Vector4(0f, 0f, 0f, 0f), new Vector4(0f, 0f, 0f, 0f), new Vector4(0f, 0f, 0f, 0f), new Vector4(0f, 0f, 0f, 0f));
         private static readonly Matrix Identity = new Matrix(new Vector4(1f, 0f, 0f, 0f), new Vector4(0f, 1f, 0f, 0f), new Vector4(0f, 0f, 1f, 0f), new Vector4(0f, 0f, 0f, 1f));
+        
+        /// <summary>
+        /// Select a index between 0 and 15
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public float this[int index]
         {
             get
             {
-                return 0;
+                switch (index)
+                {
+                    case 0:
+                        return col1.x;
+                        break;
+                    case 1:
+                        return col1.y;
+                        break;
+                    case 2:
+                        return col1.z;
+                        break;
+                    case 3:
+                        return col1.w;
+                        break;
+                    case 4:
+                        return col2.x;
+                        break;
+                    case 5:
+                        return col2.y;
+                        break;
+                    case 6:
+                        return col2.z;
+                        break;
+                    case 7:
+                        return col2.w;
+                        break;
+                    case 8:
+                        return col3.x;
+                        break;
+                    case 9:
+                        return col3.y;
+                        break;
+                    case 10:
+                        return col3.z;
+                        break;
+                    case 11:
+                        return col3.w;
+                        break;
+                    case 12:
+                        return col4.x;
+                        break;
+                    case 13:
+                        return col4.y;
+                        break;
+                    case 14:
+                        return col4.z;
+                        break;
+                    case 15:
+                        return col4.w;
+                        break;
+                    default:
+                        throw new IndexOutOfRangeException("Invalid matrix index!");
+                }
             }
             set
             {
-
+                switch (index)
+                {
+                    case 0:
+                         col1.x = value;
+                        break;
+                    case 1:
+                        col1.y = value;
+                        break;
+                    case 2:
+                        col1.z = value;
+                        break;
+                    case 3:
+                        col1.w = value;
+                        break;
+                    case 4:
+                        col2.x = value;
+                        break;
+                    case 5:
+                        col2.y = value;
+                        break;
+                    case 6:
+                        col2.z = value;
+                        break;
+                    case 7:
+                        col2.w = value;
+                        break;
+                    case 8:
+                        col3.x = value;
+                        break;
+                    case 9:
+                        col3.y = value;
+                        break;
+                    case 10:
+                        col3.z = value;
+                        break;
+                    case 11:
+                        col3.w = value;
+                        break;
+                    case 12:
+                        col4.x = value;
+                        break;
+                    case 13:
+                        col4.y = value;
+                        break;
+                    case 14:
+                        col4.z = value;
+                        break;
+                    case 15:
+                        col4.w = value;
+                        break;
+                    default:
+                        throw new IndexOutOfRangeException("Invalid matrix index!");
+                }
             }
         }
 
@@ -44,11 +154,11 @@ Matrix4x4 operator(Matrix4x4 lhs, Matrix4x4 rhs)*/
         {
             get
             {
-                return 0;
+                return this[row + column * 4];
             }
             set
             {
-
+                this[row + column * 4] = value;
             }
         }
         public static bool operator ==(Matrix lhs, Matrix rhs)
@@ -62,7 +172,7 @@ Matrix4x4 operator(Matrix4x4 lhs, Matrix4x4 rhs)*/
         public static bool operator !=(Matrix lhs, Matrix rhs) => !(lhs == rhs);
         public static Matrix operator *(Matrix lhs, Matrix rhs)
         {
-           return Zero;
+            return Zero;
         }
 
         public static Vector4 operator *(Matrix lhs, Vector4 vector)
@@ -132,7 +242,7 @@ Matrix4x4 operator(Matrix4x4 lhs, Matrix4x4 rhs)*/
 
         }
 
-        public bool ValidTRS() 
+        public bool ValidTRS()
         {
             return false;
         }
@@ -164,7 +274,7 @@ Matrix4x4 operator(Matrix4x4 lhs, Matrix4x4 rhs)*/
             }
         }
 
-        public void SetColumn(int index, Vector4 column) 
+        public void SetColumn(int index, Vector4 column)
         {
 
         }
@@ -181,7 +291,7 @@ Matrix4x4 operator(Matrix4x4 lhs, Matrix4x4 rhs)*/
             switch (index)
             {
                 case 1:
-                    return new Vector4(col1.x,col2.x,col3.x,col4.x);
+                    return new Vector4(col1.x, col2.x, col3.x, col4.x);
                     break;
                 case 2:
                     return new Vector4(col1.y, col2.y, col3.y, col4.y);
@@ -197,7 +307,7 @@ Matrix4x4 operator(Matrix4x4 lhs, Matrix4x4 rhs)*/
                     break;
             }
         }
-        public void SetRow(int index, Vector4 row) 
+        public void SetRow(int index, Vector4 row)
         {
 
         }
